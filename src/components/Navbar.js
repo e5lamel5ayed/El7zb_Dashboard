0b03e4dfc0912733d1e679c4c6aade234ef9b6df
Eslam Elsayed <111904624+e5lamel5ayed@users.noreply.github.com>
2024-03-07 16:52:13 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,76 +1,41 @@
 /* eslint-disable jsx-a11y/alt-text */
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
+import React from 'react';
+import { styled } from '@mui/material/styles';
+
+// MUI Components
 import MuiAppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
 import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
+
+// MUI Icons
 import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
+
+// Custom Hooks and Components
 import { useAppStore } from '../appStore';
 import { Link } from 'react-router-dom';
 
+
 const AppBar = styled(MuiAppBar, {
-    
-  })(({theme }) => ({
-    zIndex: theme.zIndex.drawer + 1,
-  }));
 
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.black, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.black, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
+})(({ theme }) => ({
+  zIndex: theme.zIndex.drawer + 1,
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
 
 export default function PrimarySearchAppBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-  const updateOpen = useAppStore((state)=>state.updateOpen);
-  const dopen = useAppStore((state)=>state.dopen);
+  const updateOpen = useAppStore((state) => state.updateOpen);
+  const dopen = useAppStore((state) => state.dopen);
 
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
@@ -168,7 +133,7 @@ export default function PrimarySearchAppBar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar elevation={0} position="fixed" sx={{backgroundColor: "#fff", color: "#2f2f2f"}}>
+      <AppBar elevation={0} position="fixed" sx={{ backgroundColor: "#fff", color: "#2f2f2f" }}>
         <Toolbar>
           <IconButton
             size="large"
@@ -176,26 +141,14 @@ export default function PrimarySearchAppBar() {
             color="inherit"
             aria-label="open drawer"
             sx={{ mr: 2 }}
-            onClick={()=>updateOpen(!dopen)}
+            onClick={() => updateOpen(!dopen)}
           >
             <MenuIcon />
           </IconButton>
-          {/* <Typography
-            // variant="h6"
-            // noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
-          > */}
-            <Link className='logo'>
-            
-            <img style={{
-              width:"20%",
-              // borderRadius:"50%",
-             
-              backgroundColor:"#fff"}} src='https://eladlparty.com/wp-content/uploads/2022/11/Logo-1.png' />
-              {/* <img src='https://eladlparty.com/wp-content/uploads/2022/11/Logo-1.png'/> */}
-            </Link>
-          {/* </Typography> */}
+          <Link className='logo'>
+            <img style={{ width: "20%", backgroundColor: "#fff" }}
+              src='https://eladlparty.com/wp-content/uploads/2022/11/Logo-1.png' />
+          </Link>
 
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
