1a24ec003536f33d105b7dd5de321bddc556a56d
Eslam Elsayed <111904624+e5lamel5ayed@users.noreply.github.com>
2024-03-17 16:31:13 +0200

    Add files via upload

---
+++ src/pages/Members/RoleInTheParty/MembersShow.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Sidenav from '../../../components/Sidenav';
+import NAvbar from '../../../components/Navbar';
+import Members from './Components/Members';
+import Box from "@mui/material/Box";
+
+import "../../../dash.css";
+
+
+export default function Home() {
+  return (
+    <div>
+      <NAvbar />
+
+      <div className="bgcolor">
+        <Box height={70} />
+        <Box sx={{ display: "flex" }}>
+          <Sidenav />
+          <Box component="main" sx={{ flexGrow: 1, p: 3, overflow: "auto" }}>
+
+            <Members />
+            <Box height={20} />
+          </Box>
+        </Box>
+      </div>
+    </div>
+  )
+}
