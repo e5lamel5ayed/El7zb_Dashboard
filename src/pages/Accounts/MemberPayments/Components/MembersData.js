6dd7b4ab2e0a7db5ab7168ff3ef513456b659d34
dotsy12 <113196117+dotsy12@users.noreply.github.com>
2024-03-24 16:08:18 +0200

    Add files via upload

---
+++ src/pages/Accounts/MemberPayments/Components/MembersData.js
@@ -0,0 +1,215 @@
+// import React from 'react'
+import Sidenav from "../../../../components/Sidenav";
+import Box from "@mui/material/Box";
+import NAvbar from "../../../../components/Navbar";
+
+import { Link } from "react-router-dom";
+
+export default function Settings() {
+  return (
+    <div>
+      <div className="bgcolor">
+        <NAvbar />
+        <Box height={70} />
+        <Box anchor="right" sx={{ display: "flex" }}>
+          <Sidenav />
+          <Box className="Box-style" component="main">
+            <div className="d-flex borderr">
+              <h3 className="tab3"> تسديد العضويه </h3>
+
+              <button className="btn back btn-block ">
+                <Link className="link-edit" to="/ViewPayments">
+                  رجوع
+                </Link>
+              </button>
+            </div>
+
+            <form className="form-edit">
+              <div class="form-row mb-3">
+                <div class="form-group col-md-6">
+                  <label
+                    className="d-flex"
+                    style={{
+                      fontSize: "20px",
+                      fontWeight: "600",
+                    }}
+                  >
+                    اسم العضو
+                  </label>
+                  <input type="text" class="form-control" id="inputCity" />
+                </div>
+
+                <div class="form-group col-md-6">
+                  <label
+                    className="d-flex"
+                    style={{
+                      fontSize: "20px",
+                      fontWeight: "600",
+                    }}
+                  >
+                    رقم العضوية
+                  </label>
+                  <input type="text" class="form-control" id="inputCity" />
+                </div>
+              </div>
+
+              <div class="form-row mb-3">
+                <div class="form-group col-md-4">
+                  <label
+                    className="d-flex"
+                    style={{
+                      fontSize: "20px",
+                      fontWeight: "600",
+                    }}
+                  >
+                    {" "}
+                    نوع السداد
+                  </label>
+                  <select
+                    style={{
+                      width: "100%",
+                      height: "48%",
+                      padding: "6px",
+                    }}
+                  >
+                    <optgroup>
+                      <option>نقدي </option>
+                      <option>كاش</option>
+                    </optgroup>
+                  </select>
+                </div>
+                
+                <div class="form-group col-md-4">
+                  <label
+                    className="d-flex"
+                    style={{
+                      fontSize: "20px",
+                      fontWeight: "600",
+                    }}
+                  >
+                    رقم الايصال
+                  </label>
+                  <input type="text" class="form-control" id="inputCity" />
+                </div>
+                
+                
+                <div class="form-group col-md-4">
+                  <label
+                    className="d-flex"
+                    style={{
+                      fontSize: "20px",
+                      fontWeight: "600",
+                    }}
+                  >
+                    نوع العضويه
+                  </label>
+                  <input type="text" class="form-control" id="inputCity" />
+                </div>
+
+                
+
+              </div>
+
+              <div class="form-row mb-3">
+
+              <div class="form-group col-md-4">
+                  <label
+                    className="d-flex"
+                    style={{
+                      fontSize: "20px",
+                      fontWeight: "600",
+                    }}
+                  >
+                    اسم البند
+                  </label>
+                  <input type="text" class="form-control" id="inputCity" />
+                </div>
+                
+
+                <div class="form-group col-md-4">
+                  <label
+                    className="d-flex"
+                    style={{
+                      fontSize: "20px",
+                      fontWeight: "600",
+                    }}
+                  >
+                    القيمه
+                  </label>
+                  <input type="text" class="form-control" id="inputCity" />
+                </div>
+
+                <div class="form-group col-md-4">
+                  <label
+                    className="d-flex"
+                    style={{
+                      fontSize: "20px",
+                      fontWeight: "600",
+                    }}
+                  >
+                    تاريخ التجديد
+                  </label>
+                  <input type="date" class="form-control" id="inputCity" />
+                </div>
+              </div>
+
+              <div class="form-row mb-3">
+
+              <div class="form-group col-md-4">
+                  <label
+                    className="d-flex"
+                    style={{
+                      fontSize: "20px",
+                      fontWeight: "600",
+                    }}
+                  >
+                    اسم المستلم
+                  </label>
+                  <input type="text" class="form-control" id="inputCity" />
+                </div>
+                
+
+                <div class="form-group col-md-4">
+                  <label
+                    className="d-flex"
+                    style={{
+                      fontSize: "20px",
+                      fontWeight: "600",
+                    }}
+                  >
+                    الخزنه
+                  </label>
+                  <input type="text" class="form-control" id="inputCity" />
+                </div>
+
+                <div class="form-group col-md-4">
+                  <label
+                    className="d-flex"
+                    style={{
+                      fontSize: "20px",
+                      fontWeight: "600",
+                    }}
+                  >
+                     حاله العضويه
+                  </label>
+                  <input type="text" class="form-control" id="inputCity" />
+                </div>
+              </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <button type="submit" className="save">
+                  حفظ
+                </button>
+              </div>
+            </form>
+          </Box>
+        </Box>
+      </div>
+    </div>
+  );
+}
